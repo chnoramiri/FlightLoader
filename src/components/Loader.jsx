9382956b26326719ca7eb8833633eeb2c8ebe9b2
chnoramiri <amiri.chnor@gmail.com>
2023-03-16 23:15:19 +0100

    Initial commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assests/Loader.css';
+import AirplaneIcon from '@mui/icons-material/AirplanemodeActive';
+
+const Loader = () => {
+  return <div className="loader">
+    <AirplaneIcon className="airplane"></AirplaneIcon>
+  </div>;
+};
+
+export default Loader;
